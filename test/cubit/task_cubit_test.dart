10d6c202a4bfe2cd553ac02463889d27d96ccc67
Jeff Diederiks <jeffdiederiks@Jeffs-Mac-mini.fios-router.home>
2023-12-28 03:49:22 -0500

    Set up the TaskCubit and BackendClient for success with unit testing

--- test/cubit/task_cubit_test.dart
+++ test/cubit/task_cubit_test.dart
@@ -9,18 +9,38 @@ main() {
   Task task = const Task(id: null, name: 'example task');
 
   group('Cubit Tests', () {
-    late MockTaskCubit mockTaskCubit = MockTaskCubit();
+    // JACKSON: I think this was the whole issue. 99% of the time, we don't want a mock of the class we are testing. The only other thing that needed fixing was the Mocktail when(). Based on the error, I bet you would have figured that out.
+    late TaskCubit taskCubit;
+    late MockTaskService mockTaskService;
+
+    setUp(() {
+      mockTaskService = MockTaskService();
+      taskCubit = TaskCubit(taskService: mockTaskService);
+    });
+
     blocTest(
       'Add Task adds to the Incompleted Tasks Array',
-      build: () => mockTaskCubit,
-      act: (cubit) async => await cubit.addTask(taskName: task),
+      setUp: () {
+        // JACKSON: This bit is needed to hop over the line 15 of TaskCubit
+        when(() => mockTaskService.addTask(taskName: task)).thenAnswer(
+          (_) async => Future.value(),
+        );
+      },
+      build: () => taskCubit,
+      act: (TaskCubit cubit) => cubit.addTask(taskName: task),
       expect: () => <TaskState>[
         TaskState(taskNames: [task], completedTasks: const [])
       ],
     );
+
     blocTest(
-      'Remove Task adds to the Completed Tasks Array',
-      build: () => mockTaskCubit,
+      'Complete Task and Delete Task adds to the Completed Tasks Array',
+      setUp: () {
+        when(() => mockTaskService.addTask(taskName: task)).thenAnswer(
+          (_) async => Future.value(),
+        );
+      },
+      build: () => taskCubit,
       act: (cubit) => cubit.addTask(taskName: task).then((_) {
         cubit.completeTask(taskName: task);
         cubit.deleteTask(taskName: task);
