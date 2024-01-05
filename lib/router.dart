10f26f417db008552890cee349525c776e4e386b
Jackson Geer <jacksongeer@gmail.com>
2024-01-04 21:58:05 -0500

    added transitons between navigation

--- lib/router.dart
+++ lib/router.dart
@@ -4,6 +4,50 @@ import 'package:task_list_app/pages/completed_tasks.dart';
 import 'package:task_list_app/pages/task_list.dart';
 import 'package:flutter/material.dart';
 
+CustomTransitionPage buildPageWithRightSlideTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage<T>(
+    key: state.pageKey,
+    child: child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(1.0, 0.0);
+      const end = Offset.zero;
+      final tween = Tween(begin: begin, end: end);
+      final offsetAnimation = animation.drive(tween);
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+  );
+}
+
+CustomTransitionPage buildPageWithLeftSlideTransition<T>({
+  required BuildContext context,
+  required GoRouterState state,
+  required Widget child,
+}) {
+  return CustomTransitionPage<T>(
+    key: state.pageKey,
+    child: child,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      const begin = Offset(-1.0, 0.0);
+      const end = Offset.zero;
+      final tween = Tween(begin: begin, end: end);
+      final offsetAnimation = animation.drive(tween);
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
+    },
+  );
+}
+
 GoRouter router({String initialLocation = '/'}) => GoRouter(
       routes: <RouteBase>[
         GoRoute(
@@ -14,15 +58,21 @@ GoRouter router({String initialLocation = '/'}) => GoRouter(
         ),
         GoRoute(
           path: '/add_task',
-          builder: (BuildContext context, GoRouterState state) {
-            return const AddTaskScreen();
-          },
+          pageBuilder: (context, state) =>
+              buildPageWithRightSlideTransition<void>(
+            context: context,
+            state: state,
+            child: const AddTaskScreen(),
+          ),
         ),
         GoRoute(
           path: '/completed_tasks',
-          builder: (BuildContext context, GoRouterState state) {
-            return const CompletedTaskScreen();
-          },
+          pageBuilder: (context, state) =>
+              buildPageWithLeftSlideTransition<void>(
+            context: context,
+            state: state,
+            child: const CompletedTaskScreen(),
+          ),
         ),
       ],
       initialLocation: initialLocation,
